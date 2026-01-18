8607974a299f0483237651308c92270978f96527
Myriem1 <mariemaaskri@gmail.com>
2026-01-18 14:30:10 +0100

    Initial commit: Generated Spring Boot Config Server project

---
+++ src/test/java/ecommerce_microservices/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce_microservices.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
